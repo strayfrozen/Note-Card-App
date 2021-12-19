e2bcf540db115ea59c82d3b942978b2c19266b6c
Stray <strayfrozen@yahoo.com>
2021-12-18 23:07:25 -0600

    added card.pug file

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello, Javascript Developer</h1>');
+app.get('/cards', (req, res) => {
+    res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is."});
 });
 
 
